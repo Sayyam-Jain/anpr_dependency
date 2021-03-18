7e862145dc112b8528ed214af7246566d7508b30
sayyam-jain <sayyamjain.in@gmail.com>
2021-03-18 16:46:49 +0530

    changed trt version from 20.09 to 20.07

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-From nvcr.io/nvidia/tensorrt:20.09-py3
+From nvcr.io/nvidia/tensorrt:20.07.1-py3
 # RUN apt-get update && apt-get install libgl1-mesa-glx -y
 COPY run.sh .
 COPY requirements.txt .
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/Sayyam-Jain/nv-codec-headers.git --branch sdk/10.0
 cd nv-codec-headers && make install && cd ..
 git clone https://github.com/Sayyam-Jain/FFmpeg.git && cd FFmpeg
-apt-get update && apt-get install -y build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev libgl1-mesa-glx
+apt-get update && apt-get install -y build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev libgl1-mesa-glx x264 libx264-dev
 # changing compute_30 to compute_35 and sm_30 to sm_35 inside the ffmpeg/configure worked
 # sed "s/compute_30/compute_35/g" >configure
 # sed "s/sm_30/sm_35/g" >configure
