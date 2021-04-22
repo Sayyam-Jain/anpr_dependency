b85677d7234e2dc66b21d92e7f9f0b373bb8098b
Sayyam <sayyamjain.in@gmail.com>
2021-04-22 14:29:57 +0530

    added docker-compose yml

--- example.py
+++ example.py
@@ -40,16 +40,18 @@ class Worker(Thread):
 		self.nvDec = nvc.PyNvDecoder(self.video_stream, self.gpu_id, self.rtsp_parameters)
 		
 		self.width, self.height = self.nvDec.Width(), self.nvDec.Height()
-		self.hwidth, self.hheight = int(self.width / 2), int(self.height / 2)
-		print(self.hwidth, self.hheight)
+		# self.hwidth, self.hheight = int(self.width / 2), int(self.height / 2)
+		self.hwidth, self.hheight = 416, 416
+		self.nvcolorgb_original = nvc.PySurfaceConverter(self.width, self.height, self.nvDec.Format(), nvc.PixelFormat.BGR, self.gpu_id)
+		self.nvDwn_original = nvc.PySurfaceDownloader(self.width, self.height, self.nvcolorgb_original.Format(), self.gpu_id)
+
 
 		self.nvCvt = nvc.PySurfaceConverter(self.width, self.height, self.nvDec.Format(), nvc.PixelFormat.YUV420, self.gpu_id)
-		# print('self.nvCvt.Format()', self.nvCvt.Format())
 		self.nvRes = nvc.PySurfaceResizer(self.hwidth, self.hheight, self.nvCvt.Format(), self.gpu_id)
 		self.nvcolor12 = nvc.PySurfaceConverter(self.hwidth, self.hheight, self.nvRes.Format(), nvc.PixelFormat.NV12, self.gpu_id)
-		self.nvcolorgb = nvc.PySurfaceConverter(self.hwidth, self.hheight, self.nvcolor12.Format(), nvc.PixelFormat.BGR, self.gpu_id)
+		self.nvcolorgb = nvc.PySurfaceConverter(self.hwidth, self.hheight, self.nvcolor12.Format(), nvc.PixelFormat.RGB, self.gpu_id)
 		self.nvDwn = nvc.PySurfaceDownloader(self.hwidth, self.hheight, self.nvcolorgb.Format(), self.gpu_id)
-		print('self.nvcolorgb.Format()', self.nvcolorgb.Format())
+
 
 	def run(self):
 		try:
@@ -57,84 +59,81 @@ class Worker(Thread):
 				try:
 					start_time = perf_counter()
 					rawSurface = self.nvDec.DecodeSingleSurface()
-					if (rawSurface.Empty()):
-						print('No more video frames')
-						break
+					if not rawSurface.Empty():
+						rawFrame_resized = self.resize_frame(rawSurface)
+						original_frame = self.original_frame(rawSurface)
+						# print('raw', rawFrame.shape)
+						try:
+							total_fps = 1/(perf_counter() - start_time)
+							self.num_frame += 1
+							# cv2.imshow('win', original_frame)
+							# cv2.waitKey(1)
+							if( 0 == self.num_frame % self.nvDec.Framerate() ):
+								print(self.thread_num, self.num_frame, original_frame.shape, 'Fps:', total_fps)
+
+						except Exception as e:
+							print('Show Error', e)
+					else:
+						pass
+
 				except nvc.HwResetException:
 					print('Continue after HW decoder was reset')
 					continue
- 
-				cvtSurface = self.nvCvt.Execute(rawSurface)
-				if (cvtSurface.Empty()):
-					print('Failed to do color conversion')
-					break
-
-				resSurface = self.nvRes.Execute(cvtSurface)
-				if (resSurface.Empty()):
-					print('Failed to resize surface')
-					break
- 				##########################
-
-				nv12Surface = self.nvcolor12.Execute(resSurface)
-				if (nv12Surface.Empty()):
-					print('Failed to do color conversion NV12')
-					break
-
-				rgbSurface = self.nvcolorgb.Execute(nv12Surface)
-				if (rgbSurface.Empty()):
-					print('Failed to do color conversion RGB')
-					break
-
-
-				##########################
-
-				rawFrame = np.ndarray(shape=(rgbSurface.HostSize()), dtype=np.uint8)
-				# success = self.nvcolorgb.DecodeSingleFrame(rgbSurface)
-				success = self.nvDwn.DownloadSingleSurface(rgbSurface, rawFrame)
-				# print('raw', rawFrame.shape)
-				try:
-					new_frame = rawFrame.reshape(self.hheight, self.hwidth, 3)
-					total_fps = 1/(perf_counter() - start_time)
-					# print(new_frame.shape)
-					# cv2.imshow('win', new_frame)
-					# cv2.waitKey(1)
-				except Exception as e:
-					print('Show Error', e)
-				# success = self.nvDec.DecodeSingleFrame(rawFrame)
-				if not (success):
-					print('Failed to download surface')
-					break
- 
-				self.num_frame += 1
-				if( 0 == self.num_frame % self.nvDec.Framerate() ):
-					print(self.thread_num, self.num_frame, rawFrame.shape, 'Fps:', total_fps)
-
-					# rawFrame.reshape(self.hwidth, self.hheight, 3)
-					# self.garbage_frame = rawFrame.copy()
-					# print(self.garbage_frame.shape)
-					# self.garbage_frame.reshape(self.hwidth, self.hheight, 3)
-					# print(rawFrame.shape)
-					# try:
-					# 	cv2.imshow('win', rawFrame)
-					# 	cv2.waitKey(1)
-					# except Exception as e:
-					# 	print('Show Error', e)
- 
+				
 		except Exception as e:
 			print(getattr(e, 'message', str(e)))
 			# decFile.close()
  
-def create_threads(thread_count):
-	source_dir = '/videos/'
-	video_paths = os.listdir(source_dir)
-	for thread_num in range(thread_count):
-		video_path = os.path.join(source_dir, video_paths[thread_num])
-		th1  = Worker(video_path, thread_num)
+	def resize_frame(self, rawSurface):
+		# Converting to YUV420 from NV12 as NV12 doesn't support resizing
+		cvtSurface = self.nvCvt.Execute(rawSurface)
+
+		# Converting to required Resolution
+		resSurface = self.nvRes.Execute(cvtSurface)
+
+		# Converting to NV12 again for CV2 mat, as NV12 to BGR/RGB is not supported
+		nv12Surface = self.nvcolor12.Execute(resSurface)
+
+		# Converting to RGB/BGR from NV12
+		rgbSurface = self.nvcolorgb.Execute(nv12Surface)
+
+		# new cpu casted frame of required shape
+		rawFrame = np.ndarray(shape=(rgbSurface.HostSize()), dtype=np.uint8)
+		success = self.nvDwn.DownloadSingleSurface(rgbSurface, rawFrame)
+
+		if success:
+			return rawFrame.reshape(self.hheight, self.hwidth, 3)
+		else:
+			return None
+
+	def original_frame(self, rawSurface):
+		# Converting to RGB/BGR from NV12
+		rgbSurface = self.nvcolorgb_original.Execute(rawSurface)
+
+		rawFrame = np.ndarray(shape=(rgbSurface.HostSize()), dtype=np.uint8)
+		success = self.nvDwn_original.DownloadSingleSurface(rgbSurface, rawFrame)
+
+		if success:
+			return rawFrame.reshape(self.height, self.width, 3)
+		else:
+			return None
+
+def create_threads(thread_count, source):
+	if thread_count == 1:
+		th1  = Worker(source, 1)
 		th1.start()
+
+	else:
+		source_dir = '/videos/'
+		video_paths = os.listdir(source_dir)
+		for thread_num in range(thread_count):
+			video_path = os.path.join(source_dir, video_paths[thread_num])
+			th1  = Worker(video_path, thread_num)
+			th1.start()
 
 if __name__ == "__main__":
 	gpu_1 = 0
 	# input_1 = '20201001000012.mp4'
 	input_1 = 'rtsp://192.168.5.23:554/user=admin&password=&channel=1&stream=0.sdp?' 
 	num_threads = 24
-	create_threads(num_threads)
+	create_threads(num_threads, input_1)
